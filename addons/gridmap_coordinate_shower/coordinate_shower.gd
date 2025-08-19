179ad454dbd9c136b11536e4abf92a2e139567df
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-08-19 22:42:41 +0200

    Showing coordinates works... for a prize

--- addons/gridmap_coordinate_shower/coordinate_shower.gd
+++ addons/gridmap_coordinate_shower/coordinate_shower.gd
@@ -5,7 +5,7 @@ var gme : GridMapEditorPlugin
 
 var font : Font
 
-var debug : bool = true
+const DEBUG : bool = true
 var debug_label : Label
 
 func _enter_tree() -> void:
@@ -17,14 +17,14 @@ func _enter_tree() -> void:
 	font = output_label.get_theme_font("font")
 	output_label.free()
 	
-	if debug:
+	if DEBUG:
 		debug_label = preload("res://addons/gridmap_coordinate_shower/coordinate_label.tscn").instantiate()
 		add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_BOTTOM, debug_label)
 	
 	set_force_draw_over_forwarding_enabled()
 
 func _exit_tree() -> void:
-	if debug:
+	if DEBUG:
 		remove_control_from_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_BOTTOM, debug_label)
 		debug_label.free()
 
@@ -32,7 +32,7 @@ func _handles(object: Object) -> bool:
 	return object is GridMap
 
 func _make_visible(visible: bool) -> void:
-	if debug:
+	if DEBUG:
 		if visible:
 			debug_label.show()
 		else:
@@ -49,26 +49,67 @@ func _get_selection_coordinate_string() -> String:
 			return str(selection.position) + " : " + str(selection.end)
 
 func _get_hover_coordinates_string() -> String:
-	var mouse_position = EditorInterface.get_editor_viewport_3d(0).get_mouse_position()
+	var viewport : SubViewport = EditorInterface.get_editor_viewport_3d(0)
+	var viewport_position : Vector2 = viewport.global_canvas_transform.get_origin()
 	
-	# Here some check whether it's in viewport?
+	var mouse_position = viewport.get_mouse_position()
+	var current_gridmap = gme.get_current_grid_map()
 	
+	var coordinates : Vector3i
 	
-	return str(mouse_position)
+	#XD
+	# emit click, get selection, then undo
+	if viewport.get_visible_rect().has_point(mouse_position + viewport_position) and current_gridmap:
+		var mouse_was_pressed : bool = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+		var current_selection : AABB
+		
+		if mouse_was_pressed:
+			var release = InputEventMouseButton.new()
+			release.pressed = false
+			release.button_index = MOUSE_BUTTON_LEFT
+			
+			current_selection = gme.get_selection()
+			Input.parse_input_event(release)
+			gme.clear_selection()
+		
+		var click = InputEventMouseButton.new()
+		click.global_position = viewport_position
+		click.position = mouse_position + Vector2(290, 120)
+		click.pressed = true
+		click.button_index = MOUSE_BUTTON_LEFT
+		Input.parse_input_event(click)
+		
+		var release = click.duplicate()
+		release.pressed = false
+		Input.parse_input_event(release)
+		
+		coordinates = gme.get_selection().position
+		print(coordinates)
+		
+		var undo : InputEventAction = InputEventAction.new()
+		undo.action = "ui_undo"
+		undo.pressed = true
+		Input.parse_input_event(undo)
+		
+		if mouse_was_pressed:
+			var press = click.duplicate()
+			Input.parse_input_event(press)
+			
+			gme.set_selection(current_selection.position.min(coordinates),
+				abs(current_selection.position - Vector3(coordinates)))
+		
+		return str(coordinates)
+	
+	return " "
 
 func _process(delta: float) -> void:
-	if debug:
+	update_overlays()
+	if DEBUG:
 		debug_label.text = _get_selection_coordinate_string()
 
 func _forward_3d_force_draw_over_viewport(overlay : Control):
-	overlay.draw_string_outline(font, Vector2(5, overlay.size.y - 5) ,_get_hover_coordinates_string(), 
+	overlay.draw_string_outline(font, Vector2(5, overlay.size.y - 5) , _get_hover_coordinates_string(), 
 		HORIZONTAL_ALIGNMENT_LEFT, -1, 14, 2, Color.BLACK)
-	overlay.draw_string(font, Vector2(5, overlay.size.y - 5) ,_get_hover_coordinates_string(), 
+	overlay.draw_string(font, Vector2(5, overlay.size.y - 5) , _get_hover_coordinates_string(), 
 		HORIZONTAL_ALIGNMENT_LEFT, -1, 14)
 	
-func _forward_3d_gui_input(camera, event):
-	if event is InputEventMouseMotion:
-		# Redraw viewport when cursor is moved.
-		update_overlays()
-		return EditorPlugin.AFTER_GUI_INPUT_STOP
-	return EditorPlugin.AFTER_GUI_INPUT_PASS
