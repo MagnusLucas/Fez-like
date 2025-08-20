067e86b95c705c738a709ed278fb8ae1377ed997
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-08-21 01:57:55 +0200

    Conforming to style guideline

--- addons/gridmap_coordinate_shower/coordinate_shower.gd
+++ addons/gridmap_coordinate_shower/coordinate_shower.gd
@@ -1,11 +1,8 @@
 @tool
 extends EditorPlugin
 
-var gme : GridMapEditorPlugin
-
-var debug_label : Label
-
-const BOTTOM_LOCATION = "@VBoxContainer@15/DockHSplitLeftL/DockHSplitLeftR/DockHSplitMain/@VBoxContainer@26/DockVSplitCenter/@EditorBottomPanel@7930"
+const MAIN_SPLIT = "@VBoxContainer@15/DockHSplitLeftL/DockHSplitLeftR/DockHSplitMain"
+const BOTTOM_LOCATION = MAIN_SPLIT + "/@VBoxContainer@26/DockVSplitCenter/@EditorBottomPanel@7930"
 const GRIDMAP_LOCATION = BOTTOM_LOCATION + "/@VBoxContainer@7915/@GridMapEditor@21369"
 
 const SPINBOX_LOCATION = GRIDMAP_LOCATION + "/@HBoxContainer@21316/@SpinBox@21356"
@@ -13,8 +10,11 @@ const SPINBOX_LOCATION = GRIDMAP_LOCATION + "/@HBoxContainer@21316/@SpinBox@2135
 # But it's empty...
 const AXIS_POPUP_LOCATION = GRIDMAP_LOCATION + "/@HBoxContainer@21316/@MenuButton@21364"
 
+var gme : GridMapEditorPlugin
+var output_label : Label
 var viewport_3d : SubViewport
 var spinbox : SpinBox
+
 
 func _enter_tree() -> void:
 	# For getting gridmap and its selection
@@ -22,43 +22,51 @@ func _enter_tree() -> void:
 	gme = root.find_children("", "GridMapEditorPlugin", true, false)[0]
 	
 	# For printing output in viewport
-	debug_label = preload("res://addons/gridmap_coordinate_shower/coordinate_label.tscn").instantiate()
+	output_label = preload("res://addons/gridmap_coordinate_shower/coordinate_label.tscn").instantiate()
 	viewport_3d = EditorInterface.get_editor_viewport_3d(0)
-	viewport_3d.add_child(debug_label)
+	viewport_3d.add_child(output_label)
 	
 	# For retrieving floor value of gridmap editor
 	spinbox = EditorInterface.get_base_control().get_node(SPINBOX_LOCATION)
-	
 
-func _exit_tree() -> void:
-	viewport_3d.remove_child(debug_label)
-	debug_label.free()
+
+func _process(delta: float) -> void:
+	output_label.text = _get_hover_coordinates_string()
+
 
 func _handles(object: Object) -> bool:
 	return object is GridMap
 
+
 func _make_visible(visible: bool) -> void:
 	if visible:
-		debug_label.show()
+		output_label.show()
 	else:
-		debug_label.hide()
+		output_label.hide()
 
-func _get_selection_coordinate_string() -> String:
-	if !gme.has_selection():
-		return "No active selection"
-	else:
-		var selection : AABB = gme.get_selection()
-		if selection.size == Vector3.ZERO:
-			return str(selection.position)
-		else:
-			return str(selection.position) + " : " + str(selection.end)
+
+func _exit_tree() -> void:
+	viewport_3d.remove_child(output_label)
+	output_label.free()
+
+
+#func _get_selection_coordinate_string() -> String:
+	#if !gme.has_selection():
+		#return "No active selection"
+	#else:
+		#var selection : AABB = gme.get_selection()
+		#if selection.size == Vector3.ZERO:
+			#return str(selection.position)
+		#else:
+			#return str(selection.position) + " : " + str(selection.end)
+
 
 func _get_gridmap_plane_intersection(plane_basis : Vector3, gridmap : GridMap, 
 		ray_origin : Vector3, ray_normal : Vector3) -> Variant:
 	
 	var gridmap_transform : Transform3D = gridmap.transform
-	var plane : Plane = Plane(plane_basis, 
-		gridmap_transform.origin + plane_basis * spinbox.value * gridmap.cell_size)
+	var plane := Plane(plane_basis, 
+			gridmap_transform.origin + plane_basis * spinbox.value * gridmap.cell_size)
 	
 	var intersection_point : Variant = plane.intersects_ray(ray_origin, ray_normal)
 	if intersection_point:
@@ -66,22 +74,21 @@ func _get_gridmap_plane_intersection(plane_basis : Vector3, gridmap : GridMap,
 	else:
 		return null
 
+
 func _format_intersection_string(axis : String, coordinates : Variant) -> String:
 	if coordinates != null:
 		return axis + " " + str(coordinates) + "   "
 	else:
 		return axis + " () "
 
+
 func _get_hover_coordinates_string() -> String:
-	# Casting ray from mouse position
 	var mouse_position = viewport_3d.get_mouse_position()
-	var camera = viewport_3d.get_camera_3d()
-	
 	var current_gridmap = gme.get_current_grid_map()
 	
 	if current_gridmap and viewport_3d.get_visible_rect().has_point(mouse_position):
 		var gridmap_transform : Transform3D = current_gridmap.transform
-		
+		var camera = viewport_3d.get_camera_3d()
 		var result_string : String = ""
 		
 		# TODO: Fix this to print only currently edited axis
@@ -89,23 +96,18 @@ func _get_hover_coordinates_string() -> String:
 		# But feel free to comment out any axis below as needed
 		
 		result_string += _format_intersection_string("X", _get_gridmap_plane_intersection(
-			gridmap_transform.basis.x, current_gridmap,
-			camera.project_ray_origin(mouse_position), camera.project_ray_normal(mouse_position)
+				gridmap_transform.basis.x, current_gridmap,
+				camera.project_ray_origin(mouse_position), camera.project_ray_normal(mouse_position)
 		))
-		
 		result_string += _format_intersection_string("Y", _get_gridmap_plane_intersection(
-			gridmap_transform.basis.y, current_gridmap,
-			camera.project_ray_origin(mouse_position), camera.project_ray_normal(mouse_position)
+				gridmap_transform.basis.y, current_gridmap,
+				camera.project_ray_origin(mouse_position), camera.project_ray_normal(mouse_position)
 		))
-		
 		result_string += _format_intersection_string("Z", _get_gridmap_plane_intersection(
-			gridmap_transform.basis.z, current_gridmap,
-			camera.project_ray_origin(mouse_position), camera.project_ray_normal(mouse_position)
+				gridmap_transform.basis.z, current_gridmap,
+				camera.project_ray_origin(mouse_position), camera.project_ray_normal(mouse_position)
 		))
 		
 		return result_string
 	
-	return " "
-
-func _process(delta: float) -> void:
-	debug_label.text = _get_hover_coordinates_string()
+	return ""
