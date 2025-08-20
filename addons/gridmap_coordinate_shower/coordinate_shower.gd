4bb423b09b93739517d600cc03986b5de4f208de
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2025-08-20 23:22:37 +0200

    Cleaned up code and found SpinBox with floor value

--- addons/gridmap_coordinate_shower/coordinate_shower.gd
+++ addons/gridmap_coordinate_shower/coordinate_shower.gd
@@ -3,45 +3,45 @@ extends EditorPlugin
 
 var gme : GridMapEditorPlugin
 
-var font : Font
-
-const DEBUG : bool = true
 var debug_label : Label
 
+const BOTTOM_LOCATION = "@VBoxContainer@15/DockHSplitLeftL/DockHSplitLeftR/DockHSplitMain/@VBoxContainer@26/DockVSplitCenter/@EditorBottomPanel@7930"
+const GRIDMAP_LOCATION = BOTTOM_LOCATION + "/@VBoxContainer@7915/@GridMapEditor@21369"
+const SPINBOX_LOCATION = GRIDMAP_LOCATION + "/@HBoxContainer@21316/@SpinBox@21356"
+
+var spinbox : SpinBox
+var viewport_3d : SubViewport
 
 func _enter_tree() -> void:
-	# Initialization of the plugin goes here.
+	# For getting selection
 	var root = get_editor_interface().get_resource_filesystem().get_node("/root")
 	gme = root.find_children("", "GridMapEditorPlugin", true, false)[0]
 	
-	var output_label : Label = Label.new()
-	font = output_label.get_theme_font("font")
-	output_label.free()
+	# For printing output in viewport
+	debug_label = preload("res://addons/gridmap_coordinate_shower/coordinate_label.tscn").instantiate()
+	viewport_3d = EditorInterface.get_editor_viewport_3d(0)
+	viewport_3d.add_child(debug_label)
 	
-	if DEBUG:
-		debug_label = preload("res://addons/gridmap_coordinate_shower/coordinate_label.tscn").instantiate()
-		add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_BOTTOM, debug_label)
+	# For retrieving floor value of gridmap editor
+	spinbox = EditorInterface.get_base_control().get_node(SPINBOX_LOCATION)
 	
-	set_force_draw_over_forwarding_enabled()
 
 func _exit_tree() -> void:
-	if DEBUG:
-		remove_control_from_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_BOTTOM, debug_label)
-		debug_label.free()
+	viewport_3d.remove_child(debug_label)
+	debug_label.free()
 
 func _handles(object: Object) -> bool:
 	return object is GridMap
 
 func _make_visible(visible: bool) -> void:
-	if DEBUG:
-		if visible:
-			debug_label.show()
-		else:
-			debug_label.hide()
+	if visible:
+		debug_label.show()
+	else:
+		debug_label.hide()
 
 func _get_selection_coordinate_string() -> String:
 	if !gme.has_selection():
-		return ""
+		return "No active selection"
 	else:
 		var selection : AABB = gme.get_selection()
 		if selection.size == Vector3.ZERO:
@@ -50,71 +50,15 @@ func _get_selection_coordinate_string() -> String:
 			return str(selection.position) + " : " + str(selection.end)
 
 func _get_hover_coordinates_string() -> String:
-	var viewport : SubViewport = EditorInterface.get_editor_viewport_3d(0)
-	var viewport_position : Vector2 = viewport.get_parent().global_position
+	var viewport_position : Vector2 = viewport_3d.get_parent().global_position
 	
-	var mouse_position = viewport.get_mouse_position()
+	var mouse_position = viewport_3d.get_mouse_position()
 	var current_gridmap = gme.get_current_grid_map()
 	
-	var coordinates : Vector3i
-	
-	#XD
-	# emit click, get selection, then undo
-	if viewport.get_visible_rect().has_point(mouse_position) and current_gridmap:
-		var mouse_was_pressed : bool = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
-		var current_selection : AABB
-		
-		if mouse_was_pressed:
-			var release = InputEventMouseButton.new()
-			release.pressed = false
-			release.button_index = MOUSE_BUTTON_LEFT
-			
-			current_selection = gme.get_selection()
-			Input.parse_input_event(release)
-			gme.clear_selection()
-		
-		var click = InputEventMouseButton.new()
-		click.global_position = viewport_position + mouse_position
-		click.position = viewport_position + mouse_position
-		click.pressed = true
-		click.button_index = MOUSE_BUTTON_LEFT
-		Input.parse_input_event(click)
-		
-		var release = click.duplicate()
-		release.pressed = false
-		click.global_position = viewport_position + mouse_position
-		click.position = viewport_position + mouse_position
-		Input.parse_input_event(release)
-		
-		coordinates = gme.get_selection().position
-		#gme.clear_selection()
-		
-		#var undo : InputEventAction = InputEventAction.new()
-		#undo.action = "ui_undo"
-		#undo.pressed = true
-		#Input.parse_input_event(undo)
-		
-		if mouse_was_pressed:
-			var press : InputEventMouseButton = click.duplicate()
-			Input.parse_input_event(press)
-			print(current_selection, "  ", coordinates, current_selection.position.min(coordinates), 
-				current_selection.position.min(coordinates) + abs(current_selection.position - Vector3(coordinates)))
-			gme.set_selection(current_selection.position.min(coordinates),
-				current_selection.position.min(coordinates) + abs(current_selection.position - Vector3(coordinates)))
-		
-		return str(coordinates)
+	if current_gridmap:
+		return str(current_gridmap.position)
 	
 	return " "
 
 func _process(delta: float) -> void:
-	update_overlays()
-	if DEBUG:
-		debug_label.text = _get_selection_coordinate_string()
-
-func _forward_3d_force_draw_over_viewport(overlay : Control):
-	var coordinates : String = _get_hover_coordinates_string()
-	overlay.draw_string_outline(font, Vector2(5, overlay.size.y - 5) , coordinates, 
-		HORIZONTAL_ALIGNMENT_LEFT, -1, 14, 2, Color.BLACK)
-	overlay.draw_string(font, Vector2(5, overlay.size.y - 5) , coordinates, 
-		HORIZONTAL_ALIGNMENT_LEFT, -1, 14)
-	
+	debug_label.text = _get_hover_coordinates_string()
