ed6f973954b834fe1b9ddf1ca5be58a508b23094
feargrinn <tobyrogers000@gmail.com>
2025-09-04 13:50:28 +0200

    clouds finished except disappearing on rotation

--- ScriptsAndScenes/cloud_sprite_3d.gd
+++ ScriptsAndScenes/cloud_sprite_3d.gd
@@ -2,10 +2,10 @@ extends Sprite3D
 
 @export var textures: Array[Texture] = []
 
-#const SPEED : float = 0.5
-const SPEED : float = 4
+const SPEED : float = 0.2
 @onready var marker_3d_stop = $"../../Marker3DStop"
 @onready var marker_3d_start = $".."
+var scale_comp : float
 
 func _ready():
 	#position.z = get_parent().position.z
@@ -13,11 +13,11 @@ func _ready():
 	var clouds = textures
 	var picked_cloud = clouds[randi() % clouds.size()]
 	texture = picked_cloud
-	var scale_comp = randi() % 4 + 7 * 0.1
+	scale_comp = (randi() % 3 + 8) * 0.1
 	scale = Vector3(scale_comp, scale_comp, scale_comp)
-	modulate.a = scale_comp
+	modulate.a = scale_comp - 0.3
 
 func _process(delta):
-	position.x -= SPEED * delta
+	position.x -= SPEED * scale_comp * delta
 	if position.x < (marker_3d_stop.position.x - marker_3d_start.position.x):
 		queue_free()
